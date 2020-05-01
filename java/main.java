bc30c19a45d1e4ad7561a116f71a0fd92a095016
Andrew Tomko <tomand285@gmail.com>
2020-05-01 15:46:56 -0500

    P.4 Batches, Layers, and Objects https://www.youtube.com/watch?v=TEWy9vZcxW4

--- java/main.java
+++ java/main.java
@@ -3,38 +3,79 @@
 import java.util.*;
 
 public class main {
-    public static void main(String args[]){
-        List<Double> inputs = new ArrayList<Double>(Arrays.asList(1.0, 2.0, 3.0, 2.5));
-
-        List<ArrayList<Double>> weights = new ArrayList<>(Arrays.asList(
-            new ArrayList<Double>(Arrays.asList(0.2, 0.8, -0.5, 1.0)), 
-            new ArrayList<Double>(Arrays.asList(0.5, -0.91, 0.26, -0.5)), 
-            new ArrayList<Double>(Arrays.asList(-0.26, -0.27, 0.17, 0.87))));
-
-        List<Double> biases = new ArrayList<Double>(Arrays.asList(2.0, 3.0, 0.5));
-
-        List<Double> output = add(dot(weights, inputs), biases);
-
-        System.out.println(output);
-    }
-
-    private static List<Double> dot(List<ArrayList<Double>> weights, List<Double> inputs){
-        List<Double> result = new ArrayList<>();
-        for(int i= 0; i<weights.size();i++){
-            double node = 0;
-            for(int j=0;j<weights.get(i).size();j++){
-                node += weights.get(i).get(j)*inputs.get(j);
+    private static class Layer_Dense{
+        public List<ArrayList<Double>> weights;
+        public List<Double> biases;
+        private List<ArrayList<Double>> output;
+        
+        Layer_Dense(int n_inputs, int n_neurons){
+            Random rd = new Random();
+            
+            this.weights = new ArrayList<>();
+            for(int i = 0; i < n_inputs; i++){
+                ArrayList<Double> row = new ArrayList<>();
+                for(int j = 0; j < n_neurons; j++){
+                    row.add(rd.nextDouble());
+                }
+                this.weights.add(row);
             }
-            result.add(node);
+            
+            this.biases = new ArrayList<>();
+            for(int k = 0; k < n_neurons; k++){
+                this.biases.add(0.0);
+            }
         }
-        return result;
+        
+        public void forward(List<ArrayList<Double>> inputs){
+            this.output = add(dot(inputs, this.weights), this.biases);
+        }
+        
+        public List<ArrayList<Double>> getOutput(){
+            return output;
+        }
+        
+        private static List<ArrayList<Double>> dot(List<ArrayList<Double>> a, List<ArrayList<Double>> b){
+            List<ArrayList<Double>> result = new ArrayList<>();
+            for(int i= 0; i<a.size();i++){
+                ArrayList<Double> row = new ArrayList<>();
+                for(int j=0;j<b.get(0).size();j++){
+                    Double val = 0.0;
+                    for(int k=0;k<a.get(0).size();k++){
+                        val += a.get(i).get(k) * b.get(k).get(j);
+                    }
+                    row.add(val);
+                }
+                result.add(row);
+            }
+            return result;
+        }
+    
+        private static List<ArrayList<Double>> add(List<ArrayList<Double>> a, List<Double> b){
+            List<ArrayList<Double>> result = new ArrayList<>();
+            for(int i= 0; i<a.size();i++){
+                ArrayList<Double> row = new ArrayList<>();
+                for(int j=0;j<a.get(0).size();j++){
+                    row.add(a.get(i).get(j) + b.get(j));
+                }
+                result.add(row);
+            }
+            return result;
+        }
     }
+    
+    
+    public static void main(String args[]){
+        List<ArrayList<Double>> X = new ArrayList<>(Arrays.asList(
+            new ArrayList<Double>(Arrays.asList(1.0, 2.0, 3.0, 2.5)), 
+            new ArrayList<Double>(Arrays.asList(2.0, 5.0, -1.0, 2.0)), 
+            new ArrayList<Double>(Arrays.asList(-1.5, 2.7, 3.3, -0.8))));
+        
+        Layer_Dense layer1 = new Layer_Dense(4,5);
+        Layer_Dense layer2 = new Layer_Dense(5,2);
+        
+        layer1.forward(X);
+        layer2.forward(layer1.getOutput());
 
-    private static List<Double> add(List<Double> dotProduct, List<Double> biases){
-        List<Double> result = new ArrayList<>();
-        for(int i= 0; i<dotProduct.size();i++){
-            result.add(dotProduct.get(i)+biases.get(i));
-        }
-        return result;
+        System.out.println(layer2.getOutput());
     }
 }
--- node/index.js
+++ node/index.js
@@ -1,12 +1,24 @@
 const math = require("mathjs");
 
-let inputs = [1, 2, 3, 2.5]
+let X = [[1, 2, 3, 2.5],
+        [2.0, 5.0, -1.0, 2.0],
+        [-1.5, 2.7, 3.3, -0.8]];
 
-let weights = [[0.2, 0.8, -0.5, 1.0],
-                [0.5, -0.91, 0.26, -0.5],
-                [-0.26, -0.27, 0.17, 0.87]]
+class Layer_Dense{
+    constructor(n_inputs, n_neurons){
+        this.weights = math.random([n_inputs,n_neurons]);
+        this.biases = math.zeros([n_neurons]);
+    }
+    
+    forward(inputs){
+        this.output = math.add(math.multiply(inputs, this.weights), this.biases[0]);
+    }
+}
 
-let biases = [2, 3, 0.5]
+let layer1 = new Layer_Dense(4,5)
+let layer2 = new Layer_Dense(5,2)
 
-let output = math.add(math.multiply(weights, inputs), biases);
-console.log(output)
+layer1.forward(X);
+layer2.forward(layer1.output)
+
+console.log(layer2.output)
